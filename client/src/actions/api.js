2902bb0e624e79bab062edc5a7a6778c6d5d9893
Kosenkov Denis <wdenkosw@gmail.com>
2019-05-13 00:01:53 +0300

    Changes

--- client/src/actions/api.js
+++ client/src/actions/api.js
@@ -1,10 +1,10 @@
 import openSocket from "socket.io-client";
 
 const custom = parseInt(window.location.search.replace("?", ""), 10);
-console.log(custom);
+console.log(window.location.host);
 console.log(process.env.PORT);
 const port = custom || process.env.PORT || 5000;
-const socket = openSocket(`http://localhost:${port}`);
+const socket = openSocket(window.location.host);
 
 function wsEmit(action, data) {
   socket.emit(action, data);
